6296275caee92a94c1a81392773a5c7c391975d0
Zhulin Egor <142201265+Quaant@users.noreply.github.com>
2026-07-20 13:40:33 +0300

    Change operator<< return type from ostream to void

--- clang/test/compiler-course/zhulin_task1/zhulin_task1_test.cpp
+++ clang/test/compiler-course/zhulin_task1/zhulin_task1_test.cpp
@@ -12,7 +12,7 @@ void printMessage(const char *msg) {}
 class MyClass {
 public:
   // CHECK-NOT: warning: function 'operator<<' returning non-void should be marked
-  friend std::ostream &operator<<(std::ostream &os, const MyClass &) {
+  friend void &operator<<(std::ostream &os, const MyClass &) {
     return os;
   }
 };
