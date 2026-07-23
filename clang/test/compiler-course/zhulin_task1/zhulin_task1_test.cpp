fe07a5fcf050d9862366035633148754dbe6fc8a
Quaant <egor.zhulin333@gmail.com>
2026-07-23 15:10:22 +0000

    fix plugin

--- clang/test/compiler-course/zhulin_task1/zhulin_task1_test.cpp
+++ clang/test/compiler-course/zhulin_task1/zhulin_task1_test.cpp
@@ -11,47 +11,64 @@ void printMessage(const char *msg) {}
 
 [[nodiscard]] int getSafeValue() { return 100; }
 
-void testIgnoredResult() {
-  // CHECK: warning: ignoring return value of function 'getSafeValue'
-  getSafeValue();
+void testIgnoredResult()
+{
+    // CHECK: warning: ignoring return value of function 'getSafeValue'
+    getSafeValue();
 }
 
-class TestClass {
+class TestClass
+{
 public:
-  // CHECK: warning: function 'getValue' returning non-void should be marked
-  int getValue() const { return m_value; }
-  
-  void setValue(int v) { m_value = v; }
-  
-  // CHECK: warning: function 'funcWithNoSpace' returning non-void should be marked
-  int funcWithNoSpace() { return 1; }
-  
-  // CHECK: warning: function 'funcWithSpace' returning non-void should be marked
-  int funcWithSpace () { return 1; }
-  
+    // CHECK: warning: function 'getValue' returning non-void should be marked
+    int getValue() const { return m_value; }
+
+    void setValue(int v) { m_value = v; }
+
+    // CHECK: warning: function 'funcWithNoSpace' returning non-void should be marked
+    int funcWithNoSpace() { return 1; }
+
+    // CHECK: warning: function 'funcWithSpace' returning non-void should be marked
+    int funcWithSpace() { return 1; }
+
 private:
-  int m_value = 0;
+    int m_value = 0;
 };
 
-void testFunctions() {
-    getValue();            
-  alreadyMarked();       
-  
-  TestClass tc;
-  tc.getValue();         
-  tc.funcWithNoSpace(); 
-  tc.funcWithSpace();    
+void testFunctions()
+{
+    // CHECK: warning: function 'getValue' returning non-void should be marked '[[nodiscard]]'
+    getValue();
+    // CHECK: warning: ignoring return value of function 'alreadyMarked' marked '[[nodiscard]]'
+    alreadyMarked();
+
+    TestClass tc;
+    // CHECK: warning: function 'getValue' returning non-void should be marked '[[nodiscard]]'
+    tc.getValue();
+    // CHECK: warning: function 'funcWithNoSpace' returning non-void should be marked '[[nodiscard]]'
+    tc.funcWithNoSpace();
+    // CHECK: warning: function 'funcWithSpace' returning non-void should be marked '[[nodiscard]]'
+    tc.funcWithSpace();
 }
 
-void testLambda() {
-  auto lambda = []() -> int { return 42; };
-  // CHECK: warning: ignoring return value of function 'operator()'
-  lambda();
+void testLambda()
+{
+    auto lambda = []() -> int
+    { return 42; };
+    // CHECK: warning: ignoring return value of function 'operator()'
+    lambda();
 }
 
-int main() {
-  testIgnoredResult();
-  testFunctions();
-  testLambda();
-  return 0;
+void testVoidCast()
+{
+    // CHECK-NOT: warning: ignoring return value
+    (void)getSafeValue();
+}
+
+int main()
+{
+    testIgnoredResult();
+    testFunctions();
+    testLambda();
+    return 0;
 }
